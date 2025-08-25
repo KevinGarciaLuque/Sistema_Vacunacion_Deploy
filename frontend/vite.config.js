77c65c6a22020765a140890c14c460191af1de4e
KevinGarcia <109121035+KevinGarciaLuque@users.noreply.github.com>
2025-08-24 20:40:01 -0600

    CambiosVite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// 👇 El export debe ser así
 export default defineConfig({
+  // base: "./app/", para el IIS y para producción
+  base: "/", // para desarrollo con railway
   plugins: [react()],
-  base: "./", // ✅ necesario para Railway
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
 });
 
 /*import { defineConfig } from "vite";
