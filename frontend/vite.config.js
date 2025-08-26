73962ccac58f73cafc81874426d43bf8e74f7657
KevinGarcia <109121035+KevinGarciaLuque@users.noreply.github.com>
2025-08-25 23:06:39 -0600

    Vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./", // <- Esta línea soluciona las rutas para Electron
+  base: "", // <- Esta línea soluciona las rutas para Electron
 });
 
 /*import { defineConfig } from "vite";
